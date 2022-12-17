c3a494fee9bd62f8f8b04ccd7efea81fc9e03db6
Carlos Alognon <carlosalognon08@gmail.com>
2022-12-17 12:33:16 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+const API_ID = "b5f558462160da78810acd0bb997a9fd";
+
+window.navigator.geolocation.getCurrentPosition((data)=>{
+    let lat = -15.7937308;
+    let lon = -47.881569;
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_ID}`)
+    .then((res)=>{
+        console.log(res.data);
+        $('h2').text(res.data.name)
+        $('span').text(res.data.sys.country)
+        $('.degre').text(Math.round(res.data.main.temp - 273,15) + "°C")
+    })
+})
